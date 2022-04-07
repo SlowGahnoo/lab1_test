88b234907664af305e4e3cfd333df16a432c713f
local <local@localhost>
2022-04-07 11:59:19 +0300

    Hello world commit

--- gr.upatras.junit.first/src/main/java/gr/upatras/junit/first/MyMain.java
+++ gr.upatras.junit.first/src/main/java/gr/upatras/junit/first/MyMain.java
@@ -5,6 +5,7 @@ public class MyMain {
 	public static void main(String[] args) {
 		MyClass m = new MyClass();
 		int result = m.multiply(10, 5);
+		System.out.println("Hello world");
 		System.out.println("result is " + result);
 
 	}
