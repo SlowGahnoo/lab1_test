82da8e8ce236b36b021fd9090036d87d325525e3
local <local@localhost>
2022-04-07 11:53:32 +0300

    Commit Message

---
+++ gr.upatras.junit.first/src/main/java/gr/upatras/junit/first/MyMain.java
@@ -0,0 +1,12 @@
+package gr.upatras.junit.first;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		MyClass m = new MyClass();
+		int result = m.multiply(10, 5);
+		System.out.println("result is " + result);
+
+	}
+
+}
